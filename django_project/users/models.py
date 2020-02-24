63f742cc42819cc8b3132ecb767da5826bdb2a17
rahul_1179 <rahul.aasendiya@gmail.com>
2020-02-24 14:27:03 +0530

    profile section issue resolved.

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db import models
 from PIL import Image
 
 
@@ -10,8 +12,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
@@ -19,3 +21,9 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance=None, created=False, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
